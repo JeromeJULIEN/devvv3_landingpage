a97bca461c6d8859795751a7609b1eee8960aa16
JeromeJULIEN <102427619+JeromeJULIEN@users.noreply.github.com>
2023-10-04 18:30:24 +0200

    first commit

---
+++ src/app/components/InlineText.tsx
@@ -0,0 +1,22 @@
+'use client'
+import React from 'react'
+import useScrollPosition from '../../../util/scroll';
+
+
+type Props = {
+    direction : number, // have to be 1 or -1
+}
+
+const InlineText = (props: Props) => {
+    const scrollY = useScrollPosition();
+  
+    const translateXValue : number = scrollY * 0.5 * props.direction;
+  
+
+  return (
+    <h3 className='text-4xl md:text-5xl font-semibold text-white whitespace-nowrap'
+    style={{ transform: `translateX(${translateXValue}px)` }}><span className='text-black text-edge-outline'>ACTIF NUMERIQUE</span> / SMART CONTRACT / <span className='text-black text-edge-outline'>NFT</span>  /FRONT END  / <span className='text-black text-edge-outline'>IOS</span> / MARKETPLACE / <span className='text-black text-edge-outline'>APP DECENTRALISEE</span> / ACTIF NUMERIQUE </h3>
+  )
+}
+
+export default InlineText
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,37 @@
+'use client'
 import Image from 'next/image'
+import Particles from './components/particles'
+import devvv3 from "../../public/devvv3.png"
+import downArrow from "../../public/downArrow.png"
+import InlineText from './components/InlineText'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+   <main className='flex flex-col justify-center items-center gap-10 overflow-hidden'>
+    <Particles
+				className="absolute inset-0 -z-10 animate-fade-in"
+				quantity={1000}
+			/>
+    <div className='flex flex-col gap-10 justify-between items-center min-h-screen p-10 text-white'>
+      <div className='flex-grow'></div>
+      <Image src={devvv3} alt="devvv3" className='w-2/3 md:w-1/2'/>
+      <h1 className='text-lg md:text-2xl font-light tracking-widest'>blockchain | web3 | mobile app </h1>
+      <h2 className='text-neutral-400 w-3/4 md:w-2/3 text-center'>Construisez les futurs usages numériques avec les technologies du web3, de la blockchain et des applications mobiles</h2>
+      <div className='flex-grow'></div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <Image src={downArrow} alt='downArrow' width={50} height={50}/>
+    </div>
+    <div>
+      <InlineText direction={2}/>
+      <InlineText direction={-1} />
+      <InlineText direction={1}/>
+      <InlineText direction={-2} />
+    </div>
+    <p className='mt-10 text-neutral-400 w-3/4 md:w-2/3 text-center'>Développeur fullstack web3 et ios, je suis là pour pour aider à construire votre projet, que ce soit sur tout le scope technique ou bien sur une thématique spécifique.<br/>Une idée ? Une question ? Parlons-en !</p>
+    <button className='px-6 py-2 mt-10 rounded-full bg-cyan-500 text-white  text-xl'>Prendre un rdv</button>
+    <div className='bg-white w-full min-h-screen'>
+      test
+    </div>
+   </main>
   )
 }
