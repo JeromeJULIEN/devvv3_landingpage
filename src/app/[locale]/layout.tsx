84752433e51cd9e6135837d2ef02036a97af1eed
JeromeJULIEN <102427619+JeromeJULIEN@users.noreply.github.com>
2025-01-18 15:36:13 +0100

    update site header

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,31 +1,36 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Rubik } from 'next/font/google'
-import { GoogleAnalytics } from '@next/third-parties/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Rubik } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
-
-const rubik = Rubik({ subsets: ['latin'] })
+const rubik = Rubik({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'devvv3',
-  description: 'web3, blockchain and ios application development',
-}
+  title: "devvv3",
+  description: "blockchain, web3 and decentralized applications development",
+};
 
-import {NextIntlClientProvider} from 'next-intl';
-import {notFound} from 'next/navigation'; 
+import { NextIntlClientProvider } from "next-intl";
+import { notFound } from "next/navigation";
 
 export function generateStaticParams() {
-  return [{locale: 'en'}, {locale: 'fr'}];
+  return [{ locale: "en" }, { locale: "fr" }];
 }
- 
-export default async function LocaleLayout({children, params: {locale}}:{children:any,params:any}) {
+
+export default async function LocaleLayout({
+  children,
+  params: { locale },
+}: {
+  children: any;
+  params: any;
+}) {
   let messages;
   try {
     messages = (await import(`../../../messages/${locale}.json`)).default;
   } catch (error) {
     notFound();
   }
- 
+
   return (
     <html lang={locale}>
       <body className={rubik.className}>
@@ -33,7 +38,7 @@ export default async function LocaleLayout({children, params: {locale}}:{childre
           {children}
         </NextIntlClientProvider>
       </body>
-      <GoogleAnalytics gaId='G-QF624S56MY'/>
+      <GoogleAnalytics gaId="G-QF624S56MY" />
     </html>
   );
 }
